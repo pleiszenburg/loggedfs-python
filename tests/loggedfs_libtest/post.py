bd1769b209259fc08658d6e48320e90653b5cd22
Sebastian M. Ernst <ernst@pleiszenburg.de>
2017-12-11 20:32:31 +0100

    fixed comments, added header to new file

--- tests/loggedfs_libtest/post.py
+++ tests/loggedfs_libtest/post.py
@@ -6,7 +6,7 @@ LoggedFS-python
 Filesystem monitoring with Fuse and Python
 https://github.com/pleiszenburg/loggedfs-python
 
-	tests/loggedfs_libtest/test_post.py: Stuff happening after test(s)
+	tests/loggedfs_libtest/post.py: Stuff happening after test(s)
 
 	Copyright (C) 2017 Sebastian M. Ernst <ernst@pleiszenburg.de>
 
