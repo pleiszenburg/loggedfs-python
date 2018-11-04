90c44381675b5a55464330808ad614d832484f2c
Sebastian M. Ernst <ernst@pleiszenburg.de>
2018-11-04 12:46:36 +0100

    test folder becomes module - added missing init file

---
+++ tests/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+LoggedFS-python
+Filesystem monitoring with Fuse and Python
+https://github.com/pleiszenburg/loggedfs-python
+
+	tests/__init__.py: Test module init
+
+	Copyright (C) 2017-2018 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the Apache License
+Version 2 ("License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.apache.org/licenses/LICENSE-2.0
+https://github.com/pleiszenburg/loggedfs-python/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from . import lib
