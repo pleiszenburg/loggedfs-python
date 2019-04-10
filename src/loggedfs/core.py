b709cd426346eda1956b5c902928a5d8395311d0
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-04-10 14:37:12 +0200

    added support for nanosecond timestamps in log

--- src/loggedfs/core.py
+++ src/loggedfs/core.py
@@ -40,6 +40,7 @@ import pwd
 import re
 import stat
 import sys
+import time
 
 try:
 	from time import time_ns
@@ -59,6 +60,29 @@ try:
 	from fuse import __features__ as fuse_features
 except ImportError:
 	fuse_features = {}
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# LOGGING: Support nano-second timestamps
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+class _LogRecord_ns_(logging.LogRecord):
+	def __init__(self, *args, **kwargs):
+		self.created_ns = time_ns() # Fetch precise timestamp
+		super().__init__(*args, **kwargs)
+
+class _Formatter_ns_(logging.Formatter):
+	default_nsec_format = '%s,%09d'
+	def formatTime(self, record, datefmt=None):
+		if datefmt is not None: # Do not handle custom formats here ...
+			return super().formatTime(record, datefmt) # ... leave to original implementation
+		ct = self.converter(record.created_ns / 1e9)
+		t = time.strftime(self.default_time_format, ct)
+		s = self.default_nsec_format % (t, record.created_ns - (record.created_ns // 10**9) * 10**9)
+		return s
+
+logging.setLogRecordFactory(_LogRecord_ns_)
+
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # ROUTINES
@@ -361,8 +385,8 @@ class loggedfs(Operations):
 
 	def _init_logger(self, log_enabled, log_file, log_syslog, log_printprocessname):
 
-		log_formater = logging.Formatter('%(asctime)s (%(name)s) %(message)s')
-		log_formater_short = logging.Formatter('%(message)s')
+		log_formater = _Formatter_ns_('%(asctime)s (%(name)s) %(message)s')
+		log_formater_short = _Formatter_ns_('%(message)s')
 
 		self._log_printprocessname = bool(log_printprocessname)
 
