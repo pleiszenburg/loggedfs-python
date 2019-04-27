65cc968cfaea73843330c73cd81294d64a476c04
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-04-27 12:04:02 +0200

    cleaned up event wrapper: removed last bit of string formating code used for return values and status information

---
+++ src/loggedfs/_core/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+LoggedFS-python
+Filesystem monitoring with Fuse and Python
+https://github.com/pleiszenburg/loggedfs-python
+
+	src/loggedfs/_core/__init__.py: Module core init
+
+	Copyright (C) 2017-2019 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the Apache License
+Version 2 ("License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.apache.org/licenses/LICENSE-2.0
+https://github.com/pleiszenburg/loggedfs-python/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
