3a05b875e5da3841be76465b24e964499235359e
Sebastian M. Ernst <ernst@pleiszenburg.de>
2019-04-29 15:33:11 +0200

    notify infrastructure

--- src/loggedfs/_core/notify.py
+++ src/loggedfs/_core/notify.py
@@ -29,4 +29,62 @@ specific language governing rights and limitations under the License.
 # IMPORT
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+import inspect
+import os
+
+from .defaults import LOG_BUFFERS_DEFAULT
 from .ipc import receiver_manager_class
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# CLASS: NOTIFY
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+class notify_class:
+
+
+	def __init__(self,
+		directory,
+		exit_func = None, # called on exit
+		consumer_func = None, # consumes signals
+		log_buffers = LOG_BUFFERS_DEFAULT,
+		background = False # thread in background
+		):
+
+		if not isinstance(directory, str):
+			raise TypeError('directory must be of type string')
+		if not os.path.isdir(directory):
+			raise ValueError('directory must be a path to an existing directory')
+		if not os.access(directory, os.W_OK | os.R_OK):
+			raise ValueError('not sufficient permissions on directory')
+
+		if exit_func is not None and not hasattr(exit_func, '__call__'):
+			raise ValueError('exit_func must either be None or callable')
+		if consumer_func is None or not hasattr(consumer_func, '__call__'):
+			raise ValueError('consumer_func must be callable')
+		if len(inspect.signature(consumer_func).parameters.keys()) != 1:
+			raise ValueError('consumer_func must have one parameter')
+		if not isinstance(log_buffers, bool):
+			raise TypeError('log_buffers must be of type bool')
+		if not isinstance(background, bool):
+			raise TypeError('background must be of type bool')
+
+		self._directory = os.path.abspath(directory)
+		self._exit_func = exit_func
+		self._consumer_func = consumer_func
+		self._log_buffers = log_buffers
+		self._background = background
+
+		command = ['loggedfs',
+			'-f', # foreground
+			'-p', # allow other users
+			'-s', # no syslog
+			'--lib' # "hidden" library mode
+			]
+		if self._log_buffers:
+			command.append('-b') # also log read and write buffers
+
+
+	def terminate(self):
+
+		command = ['fusermount', '-u', self._directory]
